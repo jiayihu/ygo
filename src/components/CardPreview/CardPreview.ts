de7cb75f1c7c2251877de2ef569376aba9875b0d
Jiayi Hu <jiayi.ghu@gmail.com>
2018-08-11 21:10:47 +0200

    feat: make card preview dynamic

--- src/components/CardPreview/CardPreview.ts
+++ src/components/CardPreview/CardPreview.ts
@@ -7,12 +7,52 @@ templateEl.innerHTML = `
   ${template}
 `;
 
+enum Attribute {
+  name = 'name',
+  cover = 'cover'
+}
+
 class CardPreview extends HTMLElement {
+  static get observedAttributes() {
+    return ['name', 'cover'];
+  }
+
+  coverEl: HTMLImageElement | null = null;
+  nameEl: HTMLHeadingElement | null = null;
+
   constructor() {
     super();
 
     const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.appendChild(templateEl.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    if (!this.shadowRoot) return;
+
+    this.coverEl = this.shadowRoot.querySelector('.cover');
+    this.nameEl = this.shadowRoot.querySelector('.name');
+
+    const name = this.getAttribute('name');
+    const cover = this.getAttribute('cover');
+
+    if (this.nameEl && name) {
+      this.nameEl.textContent = name;
+    }
+
+    if (this.coverEl && cover) {
+      this.coverEl.src = cover;
+    }
+  }
+
+  attributeChangedCallback(attr: Attribute, oldValue: string, newValue: string) {
+    if (attr === 'name' && this.nameEl) {
+      this.nameEl.textContent = newValue;
+    }
+
+    if (attr === 'cover' && this.coverEl) {
+      this.coverEl.textContent = newValue;
+    }
   }
 }
 
--- src/components/LatestCards/LatestCards.ts
+++ src/components/LatestCards/LatestCards.ts
@@ -17,8 +17,9 @@ class LatestCards extends HTMLElement {
       const cardsTemplate = cards
         .map(
           card => `
-            <ygo-card-preview class="column is-one-third">
-              ${card.name}
+            <ygo-card-preview name="${
+              card.name
+            }" cover="https://ygohub.com/card_images/5b11b31ccffd5.jpg" class="column is-one-third">
             </ygo-card-preview>
           `
         )
