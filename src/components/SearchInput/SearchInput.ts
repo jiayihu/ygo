7e514ab32d1613ce31d036c9974ac7be87d9788f
Jiayi Hu <jiayi.ghu@gmail.com>
2018-09-04 23:38:38 +0200

    fix(SearchInput): animate spinner on Safari
    Fuck you Safari

--- src/components/SearchInput/SearchInput.ts
+++ src/components/SearchInput/SearchInput.ts
@@ -78,6 +78,7 @@ export class SearchInput extends HyperHTMLElement<State> {
 
       <div class=${`control ${this.state.isLoading ? 'is-loading' : ''}`}>
         ${this.inputEl}
+        ${this.state.isLoading ? wire()`<span class="spinner" />` : null}
       </div>
     `;
   }
